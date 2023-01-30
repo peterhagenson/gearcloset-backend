5bff44fe58d0b169004d8cc9702c60d15d20d7ae
Peter Hagenson <p.hagenson@gmail.com>
2023-01-29 19:28:33 -0600

    /register route posting to database

--- routes/register.router.js
+++ routes/register.router.js
@@ -1,15 +1,26 @@
 //const express = require('express')
 import express from "express"
 const router = express.Router()
+import User from '../models/user.js'
 
-router.post('/', (req, res) => {
+//register
+/*router.post('/', (req, res) => {
     console.log('in post credentials', req.body)
-    const message = {
-        msg: "in post credentials"
-    }
+    const credentials = req.body;
+    const message = { msg: 'In post credentials' }
     res.send(message)
-})
+})*/
 
+//const register = async (req, res) => {
+router.post('/', async (req, res) => {
+    try {
+        //const credentials = req.body;
+        const user = await User.create(req.body)
+        res.status(201).json({ user })
+    } catch (error) {
+        res.status(500).json({ msg: 'there was an error' })
+    }
+})
 
 //module.exports = router;
 export default router;
